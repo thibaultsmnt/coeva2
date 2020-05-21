c5549af4568d042660bb9e94cbd5262a42f1ae75
Thibault Simonetto <thibault.simonetto.001@student.uni.lu>
2020-05-21 15:06:00 +0200

    Correct parameters

--- src/fitness_random_search.sh
+++ src/fitness_random_search.sh
@@ -2,9 +2,9 @@
 
 #SBATCH -o %x_%j.out
 #SBATCH -N 1
-#SBATCH -n 48
+#SBATCH -n 28
 #SBATCH -p batch
-#SBATCH --time=0-0:30:00
+#SBATCH --time=0-0:40:00
 #SBATCH --qos=qos-besteffort
 #SBATCH -J FITNESS-RANDOM-SEARCH
 #SBATCH --mail-type=end,fail
