c20545feab1c57800599673b944094eae5fcd7a1
Thibault Simonetto <thibault.simonetto.001@student.uni.lu>
2020-07-03 11:08:32 +0200

    Adversarial training

--- src/10_adversarial_training.py
+++ src/10_adversarial_training.py
@@ -1,18 +1,41 @@
-from joblib import load
+from joblib import load, dump
 
+from attacks.objectives import adversarial_training
+from attacks.venus_encoder import VenusEncoder
 from utils import Pickler, in_out
+import numpy as np
+from sklearn.base import clone
 
 config = in_out.get_parameters()
 
 
 def run(
-    MODEL_PATH,
-    ATTACK_RESULTS_PATH,
+    MODEL_PATH=config["paths"]["model"],
+    RESISTANT_MODEL_PATH=config["paths"]["resistant_model"],
+    ATTACK_RESULTS_PATH=config["paths"]["attack_results"],
     TRAIN_TEST_DATA_DIR=config["dirs"]["train_test_data"],
+    THRESHOLD=config["threshold"],
 ):
     model = load(MODEL_PATH)
     model.set_params(verbose=0, n_jobs=1)
+    encoder = VenusEncoder()
 
-    Pickler.load_from_file(ATTACK_RESULTS_PATH)
+    attack_results = Pickler.load_from_file(ATTACK_RESULTS_PATH)
+    X_adv = adversarial_training(attack_results, encoder, THRESHOLD, model)
+    y_adv = np.zeros(X_adv.shape[0]) + 1
 
-    adversarial = []
+    X_train = np.load("{}/X_train.npy".format(TRAIN_TEST_DATA_DIR))
+    y_train = np.load("{}/y_train.npy".format(TRAIN_TEST_DATA_DIR))
+
+    resistant_model = clone(model)
+    resistant_model.set_params(verbose=2, n_jobs=-1)
+    resistant_model.fit(
+        np.concatenate([X_train, X_adv]), np.concatenate([y_train, y_adv])
+    )
+    resistant_model.set_params(verbose=0, n_jobs=1)
+
+    dump(resistant_model, RESISTANT_MODEL_PATH)
+
+
+if __name__ == "__main__":
+    run()
--- src/attacks/objectives.py
+++ src/attacks/objectives.py
@@ -2,8 +2,7 @@ from .venus_constraints import evaluate
 import numpy as np
 
 
-def calculate_objectives(result, encoder, threshold, model):
-
+def objectives_per_input(result, encoder, threshold, model):
     Xs = np.array(list(map(lambda x: x.X, result.pop))).astype(np.float64)
     Xs_ml = encoder.from_genetic_to_ml(result.initial_state, Xs).astype(np.float64)
     respectsConstraints = (
@@ -17,6 +16,13 @@ def calculate_objectives(result, encoder, threshold, model):
     o3 = respectsConstraints * isMisclassified
     o4 = o3 * isBigAmount
     objectives = np.array([respectsConstraints, isMisclassified, o3, o4])
+
+    return objectives
+
+
+def calculate_objectives(result, encoder, threshold, model):
+
+    objectives = objectives_per_input(result, encoder, threshold, model)
     objectives = objectives.sum(axis=1)
     objectives = (objectives > 0).astype(np.int64)
 
@@ -30,3 +36,17 @@ def calculate_success_rates(results, encoder, threshold, model):
     )
     success_rates = np.apply_along_axis(lambda x: x.sum() / x.shape[0], 0, objectives)
     return success_rates
+
+
+def adversarial_training(results, encoder, threshold, model):
+    training = []
+
+    for result in results:
+        adv_filter = objectives_per_input(result, encoder, threshold, model)[3].astype(
+            np.bool
+        )
+        Xs = np.array(list(map(lambda x: x.X, result.pop))).astype(np.float64)
+        Xs_ml = encoder.from_genetic_to_ml(result.initial_state, Xs).astype(np.float64)
+        training.append(Xs_ml[adv_filter])
+
+    return np.concatenate(training)
