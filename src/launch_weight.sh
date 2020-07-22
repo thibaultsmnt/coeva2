d78279a5ae76863223bd7d837620bd49430743c2
Thibault Simonetto <thibault.simonetto.001@student.uni.lu>
2020-07-22 11:28:20 +0200

    launch weight

--- src/launch_weight.sh
+++ src/launch_weight.sh
@@ -12,7 +12,7 @@
 
 echo "Hello from the batch queue on node ${SLURM_NODELIST} for neural architecture generation"
 module purge
-module load swenv/default-env/v1.1-20180716-production lang/Python/3.6.4-foss-2018a
+module load swenv/default-env/v1.1-20180716-production lang/Python/3.6.4-foss-2018a math/Gurobi/8.1.1-intel-2018a-Python-3.6.4
 source ../adv/bin/activate
 
 python "$@"
