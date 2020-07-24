5d12673251d3d63dff0e75c9ddb1ac1083076628
Thibault Simonetto <thibault.simonetto.001@student.uni.lu>
2020-07-24 15:27:31 +0200

    random new

--- src/attacks/constrained_random/venus_coeva2_all_problem.py
+++ src/attacks/constrained_random/venus_coeva2_all_problem.py
@@ -24,7 +24,7 @@ class VenusProblem(Problem):
 
         x_ml = self.encoder.from_genetic_to_ml(self.original, x).astype("float64")
 
-        f1 = np.random.uniform(0, 1, len(x))
+        f1 = 0
 
         constraints = venus_constraints.evaluate(x_ml)
 
