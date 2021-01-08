fde5e98143d9c2fd7525fe38628dd44d1962c253
Thibault Simonetto <thibault.simonetto.001@student.uni.lu>
2021-01-08 16:31:09 +0100

    Papernot results

--- src/15_papernot.py
+++ src/15_papernot.py
@@ -9,6 +9,8 @@ from utils.in_out import save_to_file, pickle_from_file, load_from_file
 from utils import in_out
 from attacks.papernot_attack.problem_definition import ProblemConstraints
 import copy
+import logging
+from tqdm import tqdm
 
 from attacks.papernot_attack.attack import RFAttack
 
@@ -22,7 +24,6 @@ def unique_attack(model, nb_estimators, nb_iterations, threshold, X, y):
     return adv
 
 
-
 def run(
         MODEL_PATH=config["paths"]["model"],
         X_ATTACK_CANDIDATES_PATH=config["paths"]["x_candidates"],
@@ -34,23 +35,31 @@ def run(
         NB_ITERATIONS=config["nb_iterations"],
         OBJECTIVES_PATH=config["paths"]["objectives"],
 ):
+    logging.basicConfig(level=logging.INFO)
     experiment_id = "Papernot"
 
-    print("running Papernot with config {} experiment id {}".format(config, experiment_id))
+    # ----- Load and Scale
+
+    X_initial_states = np.load(X_ATTACK_CANDIDATES_PATH)
+    if INITIAL_STATE_OFFSET > - 1:
+        X_initial_states = X_initial_states[
+                           INITIAL_STATE_OFFSET: INITIAL_STATE_OFFSET + N_INITIAL_STATE
+                           ]
+
+    logging.info(f"Attacking with {X_initial_states.shape[0]} initial states.")
+
+    # ----- Load Model
 
     model = load(MODEL_PATH)
-    model.set_params(verbose=0, n_jobs=1)
-    problem_constraints = ProblemConstraints()
-
-    X_initial_states = np.load(X_ATTACK_CANDIDATES_PATH)[
-                       INITIAL_STATE_OFFSET: INITIAL_STATE_OFFSET + N_INITIAL_STATE
-                       ]
+    model.set_params(verbose=0, n_jobs=1)    
+    
     y = np.ones(len(X_initial_states))
 
     # attack = RFAttack(model, nb_estimators=NB_ESTIMATORS, nb_iterations=NB_ITERATIONS, threshold=THRESHOLD)
-    # adv, _, _, _ = attack.generate(X_initial_states, y)
+    # attacks, _, _, _ = attack.generate(X_initial_states, y)
 
-    adv = Parallel(n_jobs=-1)(
+    # ----- Attack
+    attacks = Parallel(n_jobs=-1, verbose=0)(
         delayed(unique_attack)(
             model,
             NB_ESTIMATORS,
@@ -59,33 +68,11 @@ def run(
             initial_state,
             y[index]
 
-        )for index, initial_state in enumerate(X_initial_states)
+        ) for index, initial_state in tqdm(enumerate(X_initial_states), total=X_initial_states.shape[0])
     )
-    adv = np.array(adv)[:, 0, :]
-
-    constraints = problem_constraints.evaluate(adv)
-    constraints_violated = constraints > 0
-    constraints_violated = constraints_violated.sum(axis=1).astype(bool)
-    respectsConstraints = 1 - constraints_violated
-
-    isMisclassified = np.array(model.predict_proba(adv)[:, 1] < THRESHOLD).astype(
-        np.int64
-    )
-    isBigAmount = (adv[:, 0] >= 10000).astype(np.int64)
-
-    o3 = respectsConstraints * isMisclassified
-    o4 = o3 * isBigAmount
-    objectives = np.array([respectsConstraints, isMisclassified, o3, o4])
-    print(objectives)
-    success_rates = objectives.sum(axis=1) / objectives.shape[1]
-    Path(OBJECTIVES_PATH).parent.mkdir(parents=True, exist_ok=True)
-
-    success_rates_df = pd.DataFrame(np.array([success_rates]), columns=["o1", "o2", "o3", "o4"])
-    success_rates_df.to_csv(OBJECTIVES_PATH)
-
-    # save_to_file(objectives, OBJECTIVES_PATH)
-    print(success_rates_df)
-    # return adv, objectives
+    attacks = np.array(attacks)[:, 0, :]
+    logging.info(f"{attacks.shape[0]} attacks generated")
+    np.save(ATTACK_RESULTS_PATH, attacks)
 
 
 if __name__ == "__main__":
