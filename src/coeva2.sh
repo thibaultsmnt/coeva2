1e75d6ef3dc94f715b558fe260fd7a8adab985c9
Thibault Simonetto <thibault.simonetto.001@student.uni.lu>
2021-01-28 15:49:06 +0100

    Script run all

--- src/coeva2.sh
+++ src/coeva2.sh
@@ -6,7 +6,7 @@ python 02_1_success_rate.py ../config/04_coeva2_f4.json
 python 01_attack.py ../config/07_coeva2_all.json
 python 02_1_success_rate.py ../config/07_coeva2_all.json
 python 02_2_history.py ../config/07_coeva2_all.json
-python 12_history_view.py ../config/07_coeva2_all.json
+python 02_3_history_view.py ../config/07_coeva2_all.json
 
 python 01_attack.py ../config/08_coeva2_f1f3f4.json
 python 02_1_success_rate.py ../config/08_coeva2_f1f3f4.json
